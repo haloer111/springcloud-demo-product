29f1adca0c2f5d31f1d0403eaaf11b4ef7564278
gexiao <345249961@qq.com>
2019-06-10 23:26:23 +0800

    init

---
+++ src/main/java/com/gexiao/demo/product/SpringcloudProductApplication.java
@@ -0,0 +1,13 @@
+package com.gexiao.demo.product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringcloudProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringcloudProductApplication.class, args);
+    }
+
+}
